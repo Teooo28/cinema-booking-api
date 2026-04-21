d7a8868f47b582f3c2c649a8c752bee8c5deb841
Teooo28 <andra-teodora.aghirasimoae@s.unibuc.ro>
2026-04-21 13:58:40 +0300

    integrate Crow framework and create /movies REST API endpoint

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,46 @@
 #include <iostream>
 #include <vector>
+#include <memory>
+#include "crow.h" 
 #include "EventFactory.h"
 #include "DiscountStrategy.h"
-#include "ApiResponse.h" 
+#include "ApiResponse.h"
 
 int main() {
-    // Exemplul 1: T devine 'std::string'
-    ApiResponse<std::string> loginResponse(true, "Autentificare cu succes", "Token: ABC-123-XYZ");
-    std::cout << "--- Exemplu String ---" << std::endl;
-    loginResponse.print();
+    // initializam aplicatie web (serverul)
+    crow::SimpleApp app;
 
-    std::cout << "\n----------------------\n" << std::endl;
+    // definim ruta
+    CROW_ROUTE(app, "/movies")([]() {
+        
+        std::vector<std::unique_ptr<Event>> schedule;
 
-    // Exemplul 2: Creăm un film folosind Factory
-    auto movie = EventFactory::createEvent("3D", 2, "Avatar", 192, 30.0, 10.0);
-    movie->setDiscountStrategy(std::make_shared<StudentDiscount>());
+        // generam filme
+        auto movie1 = EventFactory::createEvent("2D", 1, "Interstellar", 169, 30.0);
+        auto movie2 = EventFactory::createEvent("3D", 2, "Avatar", 192, 30.0, 10.0);
+        movie2->setDiscountStrategy(std::make_shared<StudentDiscount>());
 
-    // T devine 'nlohmann::json' (am cerut datele filmului sub formă de json)
-    ApiResponse<nlohmann::json> movieResponse(true, "Filmul a fost gasit", movie->toJson());
+        schedule.push_back(std::move(movie1));
+        schedule.push_back(std::move(movie2));
+
+        // punem datele filmelor intr-o lista JSON
+        nlohmann::json moviesArray = nlohmann::json::array();
+        for (const auto& event : schedule) {
+            moviesArray.push_back(event->toJson());
+        }
+
+        ApiResponse<nlohmann::json> apiResponse(true, "Filmele au fost preluate cu succes", moviesArray);
+
+        // transformam raspunsul in text si ii spunem browserului ca e un JSON
+        crow::response res(apiResponse.toJson().dump());
+        res.add_header("Content-Type", "application/json");
+        return res;
+    });
+
+    std::cout << ">>> Serverul a pornit! Deschide un browser si acceseaza: http://localhost:8080/movies <<<" << std::endl;
     
-    std::cout << "--- Exemplu Obiect Complex ---" << std::endl;
-    movieResponse.print();
+    // pornim efectiv serverul pe portul 8080
+    app.port(8080).multithreaded().run();
 
     return 0;
 }
